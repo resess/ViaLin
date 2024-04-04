272f6140125f49b99d0f113c47e85e5fc5ec69fe
Khaled Ahmed <k.e.elsayed@gmail.com>
2024-04-04 12:57:18 -0700

    Mocking location in Android

--- AndroidSource/apply_code.py
+++ AndroidSource/apply_code.py
@@ -9,6 +9,7 @@ os.system(f"cp FileOutputStream.java {android_src_folder}/libcore/ojluni/src/mai
 os.system(f"cp Formatter.java {android_src_folder}/libcore/ojluni/src/main/java/java/util/Formatter.java")
 os.system(f"cp HttpEngine.java {android_src_folder}/external/okhttp/okhttp/src/main/java/com/squareup/okhttp/internal/http/HttpEngine.java")
 os.system(f"cp Intent.java {android_src_folder}/frameworks/base/core/java/android/content/Intent.java")
+os.system(f"cp LocationManager.java {android_src_folder}/frameworks/base/location/java/android/location/LocationManager.java")
 os.system(f"cp java.mk {android_src_folder}/build/make/core/java.mk")
 os.system(f"cp Map.java {android_src_folder}/libcore/ojluni/src/main/java/java/util/Map.java")
 os.system(f"cp ObjectOutputStream.java {android_src_folder}/libcore/ojluni/src/main/java/java/io/ObjectOutputStream.java")
